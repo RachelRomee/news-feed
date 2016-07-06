6df974cebd624ca117a5804e3869d9b677886dd1
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-07-06 11:05:48 +0200

    News Feed connection

--- src/NewsFeed.js
+++ src/NewsFeed.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import $ from 'jquery';
 import NewsItem from './NewsItem';
 
 class NewsFeed extends React.Component {
@@ -11,18 +12,42 @@ class NewsFeed extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.loadNews();
+  }
+
   loadNews() {
-    
+    let url = this.props.url;
+    // because you can't use this.setState() directly you need to define before
+    let component = this;
+
+    // url: google api to transform xml to json
+    $.ajax({
+       type: "GET",
+       url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
+       dataType: 'json',
+       error: function(){
+         console.log('Unable to load feed, Incorrect path or invalid feed');
+       },
+       success: function(xml){
+         component.setState({
+           title: xml.responseData.feed.title,
+           newsItems: xml.responseData.feed.entries
+         });
+       }
+     });
   }
 
   renderNewsItem(item, index) {
+    console.log(item);
+
     return (
       <NewsItem
         key={index}
         title={item.title}
         image={item.image}
         abstract={item.abstract}
-        url={item/url} />
+        link={item.link} />
     );
   }
 
