4508e9b2c1aeeec4b55b810a6b4e49859b05b523
Rachel Romee <RachelRomee@Rachels-MacBook-Pro.local>
2016-07-06 14:13:30 +0200

    import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+// from material ui
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+// internal
 import NewsFeed from './NewsFeed';
+
+injectTapEventPlugin();
 
 const feedUrl = "http://thenextweb.com/feed/";
 
 class App extends React.Component {
     render() {
         return (
-            <NewsFeed url={feedUrl}/>
+            <MuiThemeProvider>
+              <NewsFeed url={feedUrl} />
+            </MuiThemeProvider>
         );
     }
 }
